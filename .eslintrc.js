d951f3bcea7833bd54f293e52f615c3562426084
Aryan Singh <singh.aryan837@gmail.com>
2023-11-10 11:14:05 +0530

    Backend

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended"
+        
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+  
+    "rules": {
+        'eqeqeq': 'error',
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		],
+		'no-trailing-spaces': 'error',
+		'object-curly-spacing': [
+			'error', 'always'
+		],
+		'arrow-spacing': [
+			'error', { 'before': true, 'after': true }
+		],
+		'no-console': 0
+    }
+}
